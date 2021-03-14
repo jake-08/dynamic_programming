44acb47ca3a4573096b8acd0bb71cdc8b4bd356a
Jake <linweichen08@gmail.com>
2021-03-14 21:14:28 +1100

    finished fibMemo

--- fibMemo.js
+++ fibMemo.js
@@ -1,10 +1,18 @@
-// Time Complexity -> O(2^n)
+// Time Complexity -> O(2^n) Exponential time complexity
 // Space Complexity -> O(n)
 const fib = (n) => {
   if (n <= 2) return 1;
   return fib(n - 1) + fib(n - 2);
 };
 
-console.log(fib(6));
-console.log(fib(7));
-console.log(fib(8));
+// memoization
+//      js object, keys will be arg to fn, value will be the return value
+
+const fibMemo = (n, memo = {}) => {
+  if (n in memo) return memo[n]; // memo checking logic
+  if (n <= 2) return 1;
+  memo[n] = fibMemo(n - 1, memo) + fibMemo(n - 2, memo);
+  return memo[n];
+};
+
+console.log(fibMemo(50));
